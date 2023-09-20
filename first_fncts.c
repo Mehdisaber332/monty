e3d353b5a5401155a0f789ea7caf6891540303ae
Mehdisaber332 <mehdisaber332@gmail.com>
2023-09-20 03:55:53 -0700

    stack operations

--- first_fncts.c
+++ first_fncts.c
@@ -2,113 +2,75 @@
 
 
 /**
- * monty_push - A script that pushes monty function
- * @stack: Stack Argument
- * @line_number: Number Argument
+ * add_to_stack - Adds a node to the stack.
+ * @new_node: Pointer to the new node.
+ * @ln: Interger representing the line number of of the opcode.
  */
-
-void monty_push(stack_t **stack, unsigned int line_number)
+void add_to_stack(stack_t **new_node, __attribute__((unused))unsigned int ln)
 {
-	int n;
+	stack_t *tmp;
 
-	if (var.arg == NULL)
-		push_error(stack, line_number);
-
-	if (check_arg(var.arg) != 1)
-		push_error(stack, line_number);
-
-	n = atoi(var.arg);
-	if (var.mode == STACK)
+	if (new_node == NULL || *new_node == NULL)
+		exit(EXIT_FAILURE);
+	if (head == NULL)
 	{
-		add_node_stack(stack, n);
+		head = *new_node;
+		return;
 	}
-	else if (var.mode == QUEUE)
+	tmp = head;
+	head = *new_node;
+	head->next = tmp;
+	tmp->prev = head;
+}
+
+
+/**
+ * print_stack - Adds a node to the stack.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: line number of  the opcode.
+ */
+void print_stack(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	(void) line_number;
+	if (stack == NULL)
+		exit(EXIT_FAILURE);
+	tmp = *stack;
+	while (tmp != NULL)
 	{
-		add_node_queue(stack, n);
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
 	}
 }
 
 /**
- * monty_pall - A script that prints all monty functions
- * @stack: Stack Argument
- * @line_number: Number Argument
+ * pop_top - Adds a node to the stack.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: Interger representing the line number of of the opcode.
  */
-
-void monty_pall(stack_t **stack, unsigned int line_number)
+void pop_top(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp;
+	stack_t *tmp;
 
-	temp = *stack;
+	if (stack == NULL || *stack == NULL)
+		more_err(7, line_number);
 
-	while (temp)
-	{
-		printf("%d\n", temp->n);
-		temp = temp->next;
-	}
-	(void)line_number;
+	tmp = *stack;
+	*stack = tmp->next;
+	if (*stack != NULL)
+		(*stack)->prev = NULL;
+	free(tmp);
 }
 
 /**
- * monty_add - A script that prints monty add
- * @stack: Stack Argument
- * @line_number: Number Argument
+ * print_top - Prints the top node of the stack.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: Interger representing the line number of of the opcode.
  */
-void monty_add(stack_t **stack, unsigned int line_number)
+void print_top(stack_t **stack, unsigned int line_number)
 {
-	stack_t *node1, *node2;
-
-	if ((*stack) == NULL || (*stack)->next == NULL)
-	{
-		add_error(stack, line_number);
-	}
-
-	node1 = (*stack);
-	node2 = node1->next;
-	node2->n += node1->n;
-	(*stack) = node2;
-	free(node1);
-}
-
-/**
- * monty_pint - A script that prints all monty functions
- * @stack: Stack Argument
- * @line_number: Number Argument
- */
-void monty_pint(stack_t **stack, unsigned int line_number)
-{
-	stack_t *node1;
-
-	if ((*stack) == NULL)
-	{
-		pint_error(stack, line_number);
-	}
-
-	node1 = (*stack);
-	printf("%d\n", node1->n);
-}
-
-/**
- * monty_swap - A script that prints monty swap
- * @stack: Stack Argument
- * @line_number: Number Argument
- */
-
-void monty_swap(stack_t **stack, unsigned int line_number)
-
-{
-	stack_t *node1, *node2, *node3;
-
-	if ((*stack) == NULL || (*stack)->next == NULL)
-	{
-		swap_error(stack, line_number);
-	}
-	node1 = (*stack);
-	node2 = node1->next;
-	node3 = node2->next;
-
-	node2->prev = NULL;
-	node2->next = node1;
-	node1->prev = node2;
-	node1->next = node3;
-	(*stack) = node2;
+	if (stack == NULL || *stack == NULL)
+		more_err(6, line_number);
+	printf("%d\n", (*stack)->n);
 }
